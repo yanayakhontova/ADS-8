6c55af26a90abb86e71e628d83e07495b66e9527
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-05-11 14:53:28 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,16 +1,18 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
-
 class Train {
  private:
   struct Cage {
     bool light; // состояние лампочки
-    Cage *next;
+    Cage *n;
     Cage *prev;
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  int count = 1;
+  int len = 0;
   Cage *first; // точка входа в поезд (первый вагон)
+  Cage *l;
  public:
   Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
